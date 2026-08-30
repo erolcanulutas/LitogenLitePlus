033aa4546c28a619e4bf1ee87863e86e8bfdf0d0
Erol Can Ulutas <erolcanulutas@gmail.com>
2026-08-31 00:17:41 +0300

    Put the rectangle at the top of the list as well as opening on it

--- src/shapes/index.ts
+++ src/shapes/index.ts
@@ -5,12 +5,13 @@ import { HexagonShape } from "./hexagon";
 import { PentagonShape } from "./pentagon";
 import { RectangleShape } from "./rectangle";
 
+/** Order is the order the picker offers them in, and nothing indexes it. */
 export const SHAPES: ShapePlugin[] = [
+  RectangleShape,
   TriangleShape,
   CircleShape,
   HexagonShape,
   PentagonShape,
-  RectangleShape,
 ];
 
 export function getShape(id: string) {
